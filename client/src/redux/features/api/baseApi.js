a3745bd3f509a578cfae8b218e9680985af16692
Musab Al Mahi <musabalmahi53@gmail.com>
2024-03-24 16:57:16 +0600

    try to applying redux core concept

---
+++ client/src/redux/features/api/baseApi.js
@@ -0,0 +1,29 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const baseApi = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://jsonplaceholder.typicode.com",
+  }),
+  endpoints: (builder) => ({
+    getPosts: builder.query({
+      query: () => "/posts",
+    }),
+    getUsers: builder.query({
+      query: () => "/users",
+    }),
+    getPostById: builder.query({
+      query: (id) => `/posts${id}`,
+    }),
+    setPost: builder.mutation({
+      query: (post) => ({
+        url: "/posts",
+        method: "POST",
+        body: post,
+      }),
+    }),
+  }),
+});
+export const { useGetPostsQuery, useGetPostByIdQuery, useSetPostMutation } =
+  baseApi;
+export default baseApi;
