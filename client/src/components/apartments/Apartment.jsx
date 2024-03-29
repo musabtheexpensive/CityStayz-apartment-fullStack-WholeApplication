46d9ec4974006638169d2a3b599142aa01997c93
Musab Al Mahi <musabalmahi53@gmail.com>
2024-03-29 18:05:12 +0600

    load apartment data initialy by card type

--- client/src/components/apartments/Apartment.jsx
+++ client/src/components/apartments/Apartment.jsx
@@ -1,17 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import ApartmentCard from "./ApartmentCard";
 
 const Apartment = () => {
-    const {apartments,setApartments}=useState({})
-    useEffect(()=>{
-        fetch('../../../public/apartment.json')
-        .then(res=>res.json())
-        .then(data=>setApartments(data))
-    },[])
-    return (
-        <div>
-            A
-        </div>
-    );
+  const { apartments, setApartments } = useState({});
+  useEffect(() => {
+    fetch("../../../public/apartment.json")
+      .then((res) => res.json())
+      .then((data) => setApartments(data));
+  }, []);
+  return (
+    <div>
+      {apartments.map((apartment) => (
+        <ApartmentCard key={apartment.id} apartment={apartment} />
+      ))}
+    </div>
+  );
 };
 
 export default Apartment;
